75b464811fa9b0d0c2a298bbf983375241807172
Alaye13 <101848840+Alaye13@users.noreply.github.com>
2022-12-20 14:34:38 -0500

    Add files via upload

---
+++ Lexer.java
@@ -0,0 +1,151 @@
+package unit;
+
+/**
+ * Ifenna Ekwenem
+ * Collin LLoyd
+ */
+import java.util.LinkedList;
+import java.util.Queue;
+
+import framework.Token;
+import framework.TokenName;
+
+public class Lexer {
+	public static void main(String[] args) {
+		/**
+		 * Issue with code in this particular segment due to impromper testing
+		 */
+		String sentence = "let P = 1, let Q = 1, let R = 1, eval P ^ Q ^ R?";
+		Queue<Token> tokens = new Lexer().tokenize(sentence);
+		while (!tokens.isEmpty())
+			System.out.println(tokens.remove());
+	}
+	/**
+	 * Adequately Test to see if the code works
+	 * 
+	 * @param input
+	 * @return
+	 */
+
+	public Queue<Token> tokenize(String input) {
+		Queue<Token> tokens = new LinkedList<Token>();
+
+		for (int i = 0; i < input.length(); i++) {
+
+			if (Character.isWhitespace(input.charAt(i)))
+				continue;
+
+			if (input.charAt(i) == '=') {
+				tokens.add(new Token(TokenName.EQUAL));
+			} else if (input.charAt(i) == ',') {
+				tokens.add(new Token(TokenName.COMMA));
+			} else if (input.charAt(i) == '?') {
+				tokens.add(new Token(TokenName.QUESTION));
+			} else if (input.charAt(i) == '<') {
+				if (input.charAt(i + 1) == '-') {
+					if (input.charAt(i + 2) == '>') {
+						tokens.add(new Token(TokenName.DOUBLE_ARROW));
+						i += 2;
+					}
+				}
+			}
+
+			else if (input.charAt(i) == '-') {
+				if (input.charAt(i + 1) == '>') {
+					tokens.add(new Token(TokenName.ARROW));
+
+					i += 1;
+				}
+			}
+
+			else if (input.charAt(i) == (char) 39) {
+
+				tokens.add(new Token(TokenName.APOSTROPHE));
+
+			} else if (input.charAt(i) == '^') {
+				tokens.add(new Token(TokenName.CARET));
+			}
+
+			else if (input.charAt(i) == '(') {
+				tokens.add(new Token(TokenName.OPEN_PAREN));
+			} else if (input.charAt(i) == ')') {
+				tokens.add(new Token(TokenName.CLOSE_PAREN));
+			}
+			/**
+			 * This ends the checking of the token
+			 */
+
+			else if (Character.isLetter(input.charAt(i))) {
+				int lenchecker = 1;
+				while (i + lenchecker < input.length() && Character.isLetter(input.charAt(i + lenchecker)))
+
+					lenchecker++;
+				String value = input.substring(i, i + lenchecker);
+				char[] lexemeid = new char[lenchecker + 1];
+				for (int j = 0; j < lenchecker; j++)
+					lexemeid[j] = input.charAt(i + j);
+				i += lenchecker - 1;
+				boolean variablegetter = true;
+
+				/**
+				 * Iterate through the string
+				 * This was done by adequately brainstorming and fixing code to see what failed for the keyword
+				 */
+				if ((lenchecker == 1) && (lexemeid[0] == 'V' || lexemeid[0] == 'v')) {
+					
+					tokens.add(new Token(TokenName.VEE));
+					lexemeid = null;
+					variablegetter = false;
+
+				} else if ((lenchecker == 4) && (lexemeid[0] == 'E' || lexemeid[0] == 'e')
+						&& (lexemeid[1] == 'V' || lexemeid[1] == 'v') && (lexemeid[2] == 'A' || lexemeid[2] == 'a')
+						&& (lexemeid[3] == 'L' || lexemeid[3] == 'l')) {
+					tokens.add(new Token(TokenName.EVAL_KEYWORD));
+					lexemeid = null;
+					variablegetter = false;
+				}
+
+				else if ((lenchecker == 3) && (lexemeid[0] == 'L' || lexemeid[0] == 'l')
+						&& (lexemeid[1] == 'E' || lexemeid[1] == 'e') && (lexemeid[2] == 'T' || lexemeid[2] == 't')) {
+					tokens.add(new Token(TokenName.LET_KEYWORD));
+					lexemeid = null;
+					variablegetter = false;
+
+				} else if (variablegetter != false) {
+					tokens.add(new Token(TokenName.IDENTIFIER, value));
+					lexemeid = null;
+				}
+			}
+
+			/**
+			 * This is simply going to iterate and run to see how the code works
+			 */
+			else if (Character.isDigit(input.charAt(i))) {
+				int reference = 1;
+				while (i + reference < input.length() && Character.isDigit(input.charAt(i + reference)))
+					reference++;
+
+				String lexeme = input.substring(i, i + reference);
+
+				int value = Integer.parseInt(lexeme);
+
+				boolean booleandigit;
+				if (value == 1) {
+					booleandigit = true;
+				} else {
+					booleandigit = false;
+				}
+
+				tokens.add(new Token(TokenName.BOOL_LITERAL, booleandigit));
+
+				i += reference - 1;
+
+			} else {
+				tokens.add(new Token(TokenName.UNEXPECTED_INPUT));
+			}
+
+		}
+		tokens.add(new Token(TokenName.END_OF_INPUT));
+		return tokens;
+	}
+}
---
+++ Parser.java
@@ -0,0 +1,199 @@
+package unit;
+
+/***
+ * Ifenna Ekwenem
+ * Collin Lloyd
+ * Integrity Statement
+ * CS 310 Mr Reaser
+ */
+import java.util.Queue;
+import java.util.HashMap;
+import framework.Token;
+import framework.TokenName;
+
+public class Parser {
+	public static void main(String[] args) {
+		String sentence = "let P = 1, let Q = 0, eval P -> Q?";
+		boolean value = new Parser().analyze(sentence);
+		System.out.println(value);
+	}
+
+	// New token
+	
+	private Queue<Token> tokens;
+
+	HashMap <String, Boolean> lookupTable;
+
+	public boolean analyze(String input) {
+
+		tokens = new Lexer().tokenize(input);
+
+		boolean result = expression();
+		
+		return result;
+	}
+
+	private boolean accept(TokenName name) {
+		if (tokens.peek().name != name)
+			return false;
+
+		tokens.remove();
+		return true;
+	}
+
+	private boolean peek(TokenName name) {
+		return tokens.peek().name == name;
+	}
+
+	private Object expect(TokenName name) {
+		if (tokens.peek().name != name)
+			throw new RuntimeException("Expected: " + name + ", but found: " + tokens.peek().name);
+
+		return tokens.remove().value;
+	}
+
+	private boolean expression() {
+		boolean value;
+
+		if (accept(TokenName.OPEN_PAREN)) {
+			value = equivalence();
+			expect(TokenName.CLOSE_PAREN);
+		}
+
+		else {
+			value = literal();
+		}
+
+		return value;
+	}
+
+	private boolean evaluation() {
+		boolean value = !true;
+
+		if (accept(TokenName.EVAL_KEYWORD)) {
+			value = equivalence();
+			expect(TokenName.QUESTION);
+
+		}
+		return value;
+	}
+
+	@SuppressWarnings("unused")
+	private boolean program() {
+
+		lookupTable = new HashMap<String, Boolean>();
+
+		while (peek(TokenName.LET_KEYWORD)) {
+			assignment();
+		}
+		return evaluation();
+	}
+
+	private void assignment() {
+		// TODO Auto-generated method stub
+		boolean value;
+		while (peek(TokenName.LET_KEYWORD)) {
+			if (accept(TokenName.LET_KEYWORD)) {
+				
+				String str = (String) expect(TokenName.IDENTIFIER);
+				expect(TokenName.EQUAL);
+				value = equivalence();
+				
+				// put method
+				lookupTable.put(str, value);
+				
+				expect(TokenName.COMMA);
+			}
+		}
+	}
+
+	// TODO Auto-generated method stub
+	private boolean equivalence() {
+		boolean value = implication();
+
+		while (accept(TokenName.DOUBLE_ARROW)) {
+			value = value == implication();
+		}
+		
+
+		return value;
+
+	}
+
+	private String variable() {
+		boolean value = true;
+		String str = (String) expect(TokenName.IDENTIFIER);
+		boolean literal = (Boolean) expect(TokenName.IDENTIFIER);
+		value = literal;
+		return str;
+
+	}
+
+	// Boolean Literal
+
+	private boolean booleanValue() {
+		boolean value;
+		boolean literal = (Boolean) expect(TokenName.BOOL_LITERAL);
+		System.out.println(literal);
+		value = literal;
+		return value;
+
+	}
+
+	private boolean literal() {
+		boolean value;
+	if ((Boolean) accept(TokenName.BOOL_LITERAL)) {
+			value = literal();
+
+		} else {
+			value = accept(TokenName.IDENTIFIER);
+		}
+
+		return value;
+		
+		
+		
+		
+		
+	}
+
+	private boolean conjuction() {
+		boolean value = negation();
+		while (accept(TokenName.CARET))
+			value = value & negation();
+
+		return value;
+
+	}
+
+	private boolean disjunction() {
+		boolean value = conjuction();
+
+		while (accept(TokenName.VEE))
+			value = value | conjuction();
+
+		return value;
+
+	}
+
+	private boolean negation() {
+		boolean value = expression();
+
+		if (accept(TokenName.APOSTROPHE))
+			value = !value;
+
+		return value;
+
+	}
+
+	private boolean implication() {
+		boolean value = disjunction();
+
+		if (accept(TokenName.ARROW))
+			// value = implication(); // implication rule is A->B is the same A' v B
+		value = !value | disjunction();
+		
+		return value;
+	}
+
+}
